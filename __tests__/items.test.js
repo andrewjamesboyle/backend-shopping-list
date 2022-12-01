8fa268579d78834c976d5bee72f88cf2c9f30af0
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-30 17:15:06 -0800

    get all items route passes

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -43,7 +43,7 @@ describe('items', () => {
   });
   it('POST /api/v1/items creates a new shopping item with the current user', async () => {
     const [agent, user] = await registerAndLogin();
-    const newItem = { description: 'eggs', qty: 12 };
+    const newItem = { description: 'eggs', qty: 12, };
     const resp = await agent.post('/api/v1/items').send(newItem);
     expect(resp.status).toEqual(200);
     expect(resp.body).toEqual({
